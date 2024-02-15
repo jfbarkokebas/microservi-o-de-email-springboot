8d16d4a0331f4c01587536cf18963b8521c73f58
felipe <jfbarkokebas@gmail.com>
2024-02-15 14:10:11 -0300

    last commit: create function sendEmail

--- emailservice/src/main/java/com/felipe/emailservice/EmailserviceApplication.java
+++ emailservice/src/main/java/com/felipe/emailservice/EmailserviceApplication.java
@@ -8,6 +8,7 @@ public class EmailserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailserviceApplication.class, args);
+		System.out.println("*** success build ***");
 	}
 
 }
--- emailservice/src/main/java/com/felipe/emailservice/service/EmailService.java
+++ emailservice/src/main/java/com/felipe/emailservice/service/EmailService.java
@@ -1,23 +1,49 @@
 package com.felipe.emailservice.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import com.felipe.emailservice.enums.StatusEmail;
 import com.felipe.emailservice.models.EmailModel;
 import com.felipe.emailservice.repositories.EmailRepository;
 
 @Service
 public class EmailService {
-    
+
     EmailRepository emailRepository;
+    private JavaMailSender emailSender;
 
-    public EmailService(EmailRepository emailRepository) {
+    public EmailService(EmailRepository emailRepository, JavaMailSender emailSender) {
         this.emailRepository = emailRepository;
+        this.emailSender = emailSender;
     }
 
-    public void sendEmail(EmailModel emailModel) {
-        
-        
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+
+        } catch (MailException e) {
+
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+            System.out.println(e.getMessage());
+
+        }
+
+        return emailRepository.save(emailModel);
     }
 
-    
 }
